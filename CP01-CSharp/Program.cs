d108fd3d07a08510370421e9b86adf99a59576dc
IsabelleToricelli <isabelletoricelli68@gmail.com>
2025-09-08 10:46:47 -0300

    CP01-CSharp

---
+++ CP01-CSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿// Projeto de Avaliação(Revisado) -Variante 3: Cálculo de média de três notas
+// Este programa deve solicitar o nome do estudante e três notas, calcular e exibir a média.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o nome do estudante:");
+        String nome = Console.ReadLine();
+
+        double soma = 0;
+
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.WriteLine("Digite a nota " + i + ":");
+            double nota = double.Parse(Console.ReadLine());
+            soma = soma + nota;
+            //soma = 0
+            //soma = 0 + 10
+            //soma = 10
+            //soma = 10+10
+            //soma =20
+            //soma = 20+10 = 30
+
+        }
+
+        double media = soma / 3;
+    
+        Console.WriteLine("A média do estudante " + nome + " é " + media );
+    }
+}
